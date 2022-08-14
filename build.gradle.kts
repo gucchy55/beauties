44b15e21e072f242c9bf7924b0b5dbcec808e40d
gucchy55 <57739943+gucchy55@users.noreply.github.com>
2022-08-14 13:06:52 +0900

    Library updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
   application
-  id("com.diffplug.eclipse.mavencentral") version "3.34.1"
+  id("com.diffplug.eclipse.mavencentral") version "3.37.2"
   id("com.github.johnrengelman.shadow") version "7.1.2"
-  id("com.github.ben-manes.versions") version "0.40.0"
+  id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 java {
@@ -24,7 +24,7 @@ dependencies {
 }
 
 eclipseMavenCentral {
-    release("4.21.0") {
+    release("4.24.0") {
         implementationNative("org.eclipse.swt")
         implementation("org.eclipse.jface")
         useNativesForRunningPlatform()
