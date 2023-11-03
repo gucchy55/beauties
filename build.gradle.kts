7c7e973482c132fb9cd19b6ab234bddcfe6143fe
gucchy55 <57739943+gucchy55@users.noreply.github.com>
2023-11-03 21:25:01 +0900

    updated: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
   application
   id("dev.equo.p2deps") version "1.7.3"
-  id("dev.equo.ide") version "1.7.3"
   id("com.github.johnrengelman.shadow") version "8.1.1"
   id("com.github.ben-manes.versions") version "0.49.0"
 }
@@ -14,10 +13,6 @@ application {
     mainClass.set("beauties.main.BeautiesMain")
 }
 
-tasks.shadowJar {
-    isZip64 = true
-}
-
 dependencies {
   testImplementation("junit:junit:4.+")
   implementation("mysql:mysql-connector-java:latest.release")
@@ -26,11 +21,8 @@ dependencies {
 p2deps {
   into("implementation", {
     p2repo("https://download.eclipse.org/eclipse/updates/4.29/")
-    install("org.eclipse.equinox.target.categoryIU")
+    install("org.eclipse.swt.cocoa.macosx.aarch64")
+    install("org.eclipse.jface")
   })
-}
-
-equoIde {
-  p2repo("https://download.eclipse.org/eclipse/updates/4.29/")
 }
 
