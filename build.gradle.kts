ddd394f045f8344be60db394591e5ddabdacc545
gucchy55 <57739943+gucchy55@users.noreply.github.com>
2024-03-02 15:28:55 +0900

    Library updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
 plugins {
   application
-  id("dev.equo.p2deps") version "1.7.3"
+  id("dev.equo.p2deps") version "1.7.6"
   id("com.github.johnrengelman.shadow") version "8.1.1"
-  id("com.github.ben-manes.versions") version "0.49.0"
+  id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
@@ -19,10 +25,10 @@ dependencies {
 }
 
 p2deps {
-  into("implementation", {
-    p2repo("https://download.eclipse.org/eclipse/updates/4.29/")
+  into("implementation") {
+    p2repo("https://download.eclipse.org/eclipse/updates/4.30/R-4.30-202312010110/")
     install("org.eclipse.swt.cocoa.macosx.aarch64")
     install("org.eclipse.jface")
-  })
+  }
 }
 
